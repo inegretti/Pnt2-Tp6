2e7471b113fa8f393b6024f0caa1cd5dca5282bc
inegretti <88805043+inegretti@users.noreply.github.com>
2022-10-30 01:22:56 -0300

    entrega

---
+++ src/App.vue
@@ -0,0 +1,29 @@
+<template>
+  <div id="app">
+    <br>
+    <!--<Header/>-->
+    <NavBar/>
+  </div>
+</template>
+
+<script>
+//import Header from './components/Header.vue'
+import NavBar from './components/NavBar.vue'
+export default {
+  name: 'App',
+  components: {
+   // Header,
+    NavBar,
+  }
+}
+</script>
+
+<style>
+#app {
+   background: #232323;
+	margin: 0;
+	font-family: "Montserrat", "Avenir";
+}
+
+
+</style>
